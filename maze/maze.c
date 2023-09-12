425a24adc90362cc8cf9bfb1f5d2531aafeaee78
consius <144313681+consius@users.noreply.github.com>
2023-09-12 23:49:30 +0530

    Add movable character to solve the maze with

--- maze/maze.c
+++ maze/maze.c
@@ -1,30 +1,33 @@
+// lets make a command line maze game
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
+#include <locale.h>
 #include <time.h>
 
-const int maze_size_y = 20;
-const int maze_size_x = 40;
+const int maze_size_y = 10; //change maze size here
+const int maze_size_x = 20;
 const int arr_size_y = maze_size_y*2+3;
 const int arr_size_x = maze_size_x*2+3;
 const int Stack_size = arr_size_y*arr_size_x*2;
 
-char* which_brick(int grid[arr_size_y][arr_size_x], char bricks[16][4], int posy, int posx)
+int which_brick(int grid[arr_size_y][arr_size_x], int posy, int posx)
 {
     if(grid[posy][posx] == 0)
-        return " ";
-    return bricks[(8*grid[posy-1][posx]) + (4*grid[posy][posx+1]) + (2*grid[posy+1][posx]) + (grid[posy][posx-1])];
+        return 0;
+    return (8*grid[posy-1][posx]) + (4*grid[posy][posx+1]) + (2*grid[posy+1][posx]) + (grid[posy][posx-1]);
 }
 
-int show_maze(int grid[arr_size_y][arr_size_x], char bricks[16][4], int arr_size_y, int arr_size_x)
+int finalize_maze(int grid[arr_size_y][arr_size_x], int arr_size_y, int arr_size_x)
 {
+    int grid2[arr_size_y][arr_size_x];
+    for(int y = 0; y<arr_size_y; y++)
+        for(int x = 0; x<arr_size_x; x++)
+            grid2[y][x] = grid[y][x];
+
     for(int y = 1; y < arr_size_y-1; y++)
-    {
         for(int x = 1; x < arr_size_x-1; x++)
-        {
-            printf("%s", which_brick(grid, bricks, y, x));
-        }
-        printf("\n");
-    }
+            grid[y][x] = which_brick(grid2, y, x);
 
     return 0;
 }
@@ -79,8 +82,8 @@ int recursive_backtrack(int grid[arr_size_y][arr_size_x], int arr_size_y, int ar
 {
     int Stack[Stack_size];
     int top = -1;
-    int currentx = 4;
-    int currenty = 4;
+    int currentx = 2; // Where does the recusive backtrack start from
+    int currenty = 2;
     int nextx = 0;
     int nexty = 0;
     int neighbour = 0;
@@ -92,7 +95,7 @@ int recursive_backtrack(int grid[arr_size_y][arr_size_x], int arr_size_y, int ar
         if((neighbour = check_neighbour(grid, currenty, currentx, arr_size_x)))
         {
             push(Stack, &top, encode_one(currenty, currentx, arr_size_x));//pused to stack
-            decode_pair(&nexty, &nextx, arr_size_x, neighbour);//neighbour choosed
+            decode_pair(&nexty, &nextx, arr_size_x, neighbour);//neighbour chooosed
             if(currenty == nexty)// Wall removed
                 grid[currenty][currentx + ((nextx - currentx)/2)] = 4;
             if(currentx == nextx)
@@ -109,11 +112,96 @@ int recursive_backtrack(int grid[arr_size_y][arr_size_x], int arr_size_y, int ar
     return 0;
 }
 
+int won()
+{
+    clear();
+    printw("YOU WON!!!\n");
+    printw("Press any key to quit...\n");
+    getch();
+    endwin();
+    exit(0);
+}
+
+int Move(int grid[arr_size_y][arr_size_x], int posy, int posx, int posy1, int posx1, int marky, int markx)
+{
+    if(grid[posy][posx] == 0)
+    {
+        grid[posy1][posx1] = 17;
+        grid[posy][posx] = 16;
+        mvprintw(posy1+marky, posx1+markx, ".");
+        mvprintw(posy+marky, posx+markx, "*");
+        return 0;
+    }
+
+    if(grid[posy][posx] == 17)
+    {
+        grid[posy1][posx1] = 0;
+        grid[posy][posx] = 16;
+        mvprintw(posy1+marky, posx1+markx, " ");
+        mvprintw(posy+marky, posx+markx, "*");
+        return 0;
+    }
+
+    if(grid[posy][posx] == 18)
+    {
+        won();
+        return 0;
+    }
+    return 1;
+}
+
+int Input(int grid[arr_size_y][arr_size_x], int* posy, int* posx, int *marky, int *markx)
+{
+    char inpt;
+    re_inpt:
+    inpt = getch();
+
+    switch(inpt) {
+        case 'w':
+            if(!Move(grid, *posy-1, *posx, *posy, *posx, *marky, *markx))
+                *posy -= 1;
+            break;
+        case 'a':
+            if(!Move(grid, *posy, *posx-1, *posy, *posx, *marky, *markx))
+                *posx -= 1;
+            break;
+        case 's':
+            if(!Move(grid, *posy+1, *posx, *posy, *posx, *marky, *markx))
+                *posy += 1;
+            break;
+        case 'd':
+            if(!Move(grid, *posy, *posx+1, *posy, *posx, *marky, *markx))
+                *posx += 1;
+            break;
+        default:
+            goto re_inpt;
+    }
+    return 0;
+}
+
+int print_maze(char bricks[19][4], int grid[arr_size_y][arr_size_x], WINDOW *win, int *posy, int *posx)
+{
+    //printw("                     MAZE GAME\n");
+    //printw("                 Find The Way Out!\n");
+    getyx(win, *posy, *posx);
+    *posy = *posy - 1;
+    *posx = *posx - 1;
+    for(int y = 1; y<arr_size_y-1; y++)
+    {
+        for(int x = 1; x<arr_size_x-1; x++)
+            printw("%s", bricks[grid[y][x]]);
+        printw("\n");
+    }
+    return 0;
+}
+
 int main()
 {
+    setlocale(LC_ALL, "");
+    WINDOW *win = initscr(); cbreak(); noecho(); curs_set(0);
     srand(time(NULL));
     int grid[arr_size_y][arr_size_x];
-    char bricks[16][4] = {" ", "─", "│", "┐", "─", "─", "┌", "┬", "│", "┘", "│", "┤", "└", "┴", "├", "┼"};
+    char bricks[19][4] = {" ", "─", "│", "┐", "─", "─", "┌", "┬", "│", "┘", "│", "┤", "└", "┴", "├", "┼", "*", ".", "o"};
     for(int y = 0; y < arr_size_y; y++)
     {
         for(int x = 0; x < arr_size_x; x++)
@@ -126,8 +214,17 @@ int main()
                 grid[y][x] = 1;
         }
     }
-
+    int posx = 2, posy = 2;
+    int markx, marky;
     recursive_backtrack(grid, arr_size_y, arr_size_x);
-    show_maze(grid, bricks, arr_size_y, arr_size_x);
+    finalize_maze(grid, arr_size_y, arr_size_x);
+    grid[arr_size_y-3][arr_size_x-3] = 18;
+    print_maze(bricks, grid, win, &marky, &markx);
+    mvprintw(posy+marky, posx+markx, "*");
+    while(1)
+    {
+        Input(grid, &posy, &posx, &marky, &markx);
+    }
+    endwin();
     return 0;
 }
